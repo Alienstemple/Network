62bbec79cf5b768a80e64fe16833c3bb70e5e476
Alyona <borisovsk_a@mail.ru>
2023-01-26 12:35:46 +0300

    feat: Handlers working

--- app/src/main/java/com/example/network/MainActivity.kt
+++ app/src/main/java/com/example/network/MainActivity.kt
@@ -2,36 +2,74 @@ package com.example.network
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.network.databinding.ActivityMainBinding
+import com.example.network.model.Picture
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var pictureAdapter: PictureAdapter
+
+    private var pictureList: List<Picture> =
+        listOf(
+            Picture(0, 0, "Title1", "url1", "url1"),
+            Picture(0, 0, "Title2 kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk", "url2", "url2"),
+            Picture(0, 0, "Title3", "url3", "url3"))
+
+    private val backgroundHandler: Handler
+    private val uiHandler =
+        Handler(Looper.getMainLooper())  // Основной поток уже запущен. Возьмем от него looper
+
+    private val getPicturesFromNetwork = Runnable {
+        pictureList = NetworkService().getPictures()
+        uiHandler.post(updateUI)
+    }
+
+    private val updateUI = Runnable {
+        updatePictureRecycler()
+    }
+
+    init {
+        val backgroundHandlerThread = HandlerThread("background")
+        backgroundHandlerThread.start()    // Запустим фоновый поток
+        backgroundHandler =
+            Handler(backgroundHandlerThread.looper)   // В конструктор передадим looper запущенного фонового потока
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-
         mainBinding.getBtn.setOnClickListener {
-            val pictureList = NetworkService().getPictures()
-            pictureAdapter = PictureAdapter(pictureList)
-
-            mainBinding.pictureRecycle.layoutManager =
-                LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-            mainBinding.pictureRecycle.adapter = pictureAdapter
+            backgroundHandler.post(getPicturesFromNetwork)
         }
     }
 
-    companion object MainCompanion : UiUpdater{
+    override fun onDestroy() {
+        super.onDestroy()
+        backgroundHandler.removeCallbacksAndMessages(null)
+        uiHandler.removeCallbacksAndMessages(null)
+    }
+
+    fun upd() {
+        mainBinding
+    }
+
+    private fun updatePictureRecycler() {
+        pictureAdapter = PictureAdapter(pictureList)
+
+        mainBinding.pictureRecycle.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        mainBinding.pictureRecycle.adapter = pictureAdapter
+    }
+
+    companion object {
         const val TAG = "MainActLog"
         const val requestUrl = "https://jsonplaceholder.typicode.com/albums/1/photos"
-        override fun updatePictureRecycler() {
-            Log.d(TAG, "updatePicture called")
-        }
     }
-
 }
--- app/src/main/java/com/example/network/NetworkService.kt
+++ app/src/main/java/com/example/network/NetworkService.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import com.example.network.model.Picture
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.google.gson.Gson
 import okhttp3.CookieJar
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -25,26 +24,7 @@ class NetworkService {
             Picture(0, 0, "Title2 kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk", "url2", "url2"),
             Picture(0, 0, "Title3", "url3", "url3"))
 
-    private val backgroundHandler: Handler
-
-    private val getPicturesFromNetwork = Runnable {
-        getPicturesFromNetwork()
-    }
-
-    init {
-        val backgroundHandlerThread = HandlerThread("background")
-        backgroundHandlerThread.start()    // Запустим фоновый поток
-        backgroundHandler =
-            Handler(backgroundHandlerThread.looper)   // В конструктор передадим looper запущенного фонового потока
-    }
-
     fun getPictures(): List<Picture> {
-        backgroundHandler.post(getPicturesFromNetwork)
-        return pictureList
-    }
-
-    private fun getPicturesFromNetwork() {
-
         val httpClient = OkHttpClient.Builder()
             .readTimeout(3, TimeUnit.SECONDS)
             .writeTimeout(3, TimeUnit.SECONDS)
@@ -76,9 +56,7 @@ class NetworkService {
 
         Log.d(TAG, "${pictureList.indices}")
 
-        uiUpdater().updatePictureRecycler()
-
-        // TODO jackson , initialize List
+        return pictureList
     }
 
     companion object {
